9cbb0ec212306d172723d992cd9f294f62eb5fb9
chinkauchenna2021 <uchennachinka4@gmail.com>
2023-11-24 07:55:13 -0800

    add zod

--- src/controllers/RegistrationController.ts
+++ src/controllers/RegistrationController.ts
@@ -1,11 +1,27 @@
 import { PrismaClient } from "@prisma/client";
+import { IRegistrationController } from "../dto/IRegistration";
+import { ZRegistration } from "../lib/Zod";
 
-export function registration(){
-   return async function(){
+export function registration({prisma , usersInputObject}:IRegistrationController){
+    return async function(){
+        try{
+        const data = await ZRegistration.parseAsync(usersInputObject)
+
+      await prisma?.user?.create({
+             data:{
+                email:data.email,
+                password: data.password,
+                fistname:data.fistname,
+                lastname: data.lastname,
+                address: data.address,
+                pics: data.pics
+             }
+        })
+
+    }catch(error){
+      throw new Error("registration failed "+ error)
+    }
        
-
-
-
    }
 
 }
--- src/lib/Zod.ts
+++ src/lib/Zod.ts
@@ -1,9 +1,13 @@
-import {z , infer} from 'zod'
+import {z ,  } from 'zod'
 
-const ZRegistration = z.object({
-     email: z.string().email({message:""})
-    
+ export const ZRegistration = z.object({
+     email: z.string().email({message:" not a valid email "}),
+     password: z.string().min(6 , {message:"password must not be less than 5"}),
+     fistname: z.string(),
+     lastname: z.string(),
+     address: z.string().min(10 , {message:"address length too short"}),
+     pics: z.string(),
+     balance: z.number().optional()
 })
 
-
-export const regValidation = infer<typeof ZRegistration>
+ 
